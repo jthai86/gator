0ca418f76957e320cc9b1f84230c9f80b28e67ae
jthai <jthai@gmail.com>
2025-09-26 16:43:45 -0700

    first commit

---
+++ sql/schema/001_users.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    name TEXT NOT NULL UNIQUE
+);
+
+-- +goose Down
+DROP TABLE users;
